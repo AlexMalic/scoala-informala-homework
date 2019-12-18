54d87e2e67d9e21174790def74ecec6185c38f7f
Alex <alexandru_malic@yahoo.com>
2019-12-18 19:44:58 +0200

    Main Class. Updated

--- IntroToJava/src/Package/MainClass.java
+++ IntroToJava/src/Package/MainClass.java
@@ -2,6 +2,7 @@ package Package;
 
 public class MainClass {
     public static void main(String[] args) {
-        System.out.println("Acesta este un test.");
+
+        System.out.println("Acesta este un test. After readme");
     }
 }
